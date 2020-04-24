5647246be63ddf79d82067304915d40d4da30ebe
Angel Mendez <angelmendezz751@gmail.com>
2020-04-23 19:18:27 -0500

    ADD BOARD COMPONENT

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Backend from 'react-dnd-html5-backend'
 import Knight from "../src/components/Knight";
 import Head from "next/head";
 import Square from "../src/components/Square";
+import Board from "../src/components/Board";
 
 const IndexPage = () => (
     <>
@@ -10,9 +11,7 @@ const IndexPage = () => (
             <title>Drag and Drop - Azachii Components</title>
         </Head>
         <DndProvider backend={Backend}>
-            <Square black>
-                <Knight/>
-            </Square>
+            <Board/>
         </DndProvider>
     </>
 )
---
+++ src/components/Board/index.jsx
@@ -0,0 +1,12 @@
+import Square from "../Square";
+import Knight from "../Knight";
+const Board = () => {
+    return (
+        <div>
+            <Square black>
+                <Knight />
+            </Square>
+        </div>
+    )
+}
+export default Board;
