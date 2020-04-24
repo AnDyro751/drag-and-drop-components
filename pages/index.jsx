2d7a8568c83e0b8d598c69eaa3a472b143d509b7
Angel Mendez <angelmendezz751@gmail.com>
2020-04-23 19:18:53 -0500

    REMOVE index.jsx IMPORTS

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,6 @@
 import {DndProvider} from 'react-dnd'
 import Backend from 'react-dnd-html5-backend'
-import Knight from "../src/components/Knight";
 import Head from "next/head";
-import Square from "../src/components/Square";
 import Board from "../src/components/Board";
 
 const IndexPage = () => (
