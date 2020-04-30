ce9c6f99d28402f0cc59ada3dee680c4a195a759
Angel Mendez <angelmendezz751@gmail.com>
2020-04-30 17:02:32 -0500

    YA SE PUEDEN AGREGAR Y ELIMINAR COMPONENTES

--- addComponent.rb
+++ addComponent.rb
@@ -7,18 +7,39 @@ end
 
 # file = File.open(FILE_NAME, "r+")
 
-def remove_line(string, new_component = "", name_component = "", inside = false)
-  if new_component.empty?
-    raise "Invalid name component"
-  end
+def remove_line(string, replace_string, other_string_option, delete = false)
   file = File.read(FILE_NAME)
-  contained = file.downcase.include? name_component.downcase && new_component.downcase
-  unless contained
-    new_content = file.gsub(string, "STARTIMPORT\n import #{inside ? "{#{name_component}}" : "#{name_component}"} from '#{new_component}';")
-    # new_content = new_content.map { |word| word == string ? "import ClassicEnBanner from '@components/Banners/ClassicEnBanner';\n// ENDIMPORT" : word }.join("\n")
-    File.open(FILE_NAME, "w") { |line| line.puts new_content }
+  if delete
+    input_lines = File.readlines(FILE_NAME)
+    # output_lines = input_lines.select.with_index { |_, i| i.even? }
+    File.open(FILE_NAME, 'w') do |f|
+      input_lines.each do |line|
+        f.write line unless line.downcase.include? string.downcase
+      end
+    end
+  else
+    contained = file.downcase.include? replace_string.downcase
+    other_contained = file.downcase.include? other_string_option.downcase
+    unless contained || other_contained
+      new_content = file.gsub(string, replace_string)
+      File.open(FILE_NAME, "w") { |line| line.puts new_content }
+    end
   end
 end
 
-remove_line("STARTIMPORT", "@components/Banners/ClassicEnBanner", "ClassicEnBanner", false)
+def get_full_import(name_component = "", new_component = "", inside = false)
+  "STARTIMPORT\nimport #{inside ? "{#{name_component}}" : "#{name_component}"} from '#{new_component}';"
+end
+
+# puts get_full_import()
+
+
+def add_import(name_component = "", new_component = "", inside = false)
+  remove_line("STARTIMPORT", get_full_import(name_component, new_component, inside), get_full_import(name_component, new_component, !inside), false)
+  remove_line("STARTCOMPONENTS", "STARTCOMPONENTS\n #{name_component}: #{name_component},", "#{name_component}: #{name_component},", false)
+end
+
+
+# add_import("ClassicEnasaBanner", "@components/Banners/ClassicEaskmnBanner", false)
+# remove_line("ClassicEnBanner", "", "", true)
 # file.close
--- src/components/GetComponents/importer.js
+++ src/components/GetComponents/importer.js
@@ -1,8 +1,10 @@
 // STARTIMPORT
- import ClassicEnBanner from '@components/Banners/ClassicEnBanner';
+import ClassicEnasaBanner from '@components/Banners/ClassicEaskmnBanner';
+import ClassicEnBanner from '@components/Banners/ClassicEnBanner';
+
 const Components = {
+    // STARTCOMPONENTS
+    ClassicEnasaBanner: ClassicEnasaBanner,
     ClassicEnBanner: ClassicEnBanner,
 }
-
-
 export default Components
